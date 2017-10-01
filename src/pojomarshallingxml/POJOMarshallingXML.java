836f8487e89b6370ea1a6695be44728ec4ba89d3
Ángel Millán Jaén <angelmillan@me.com>
2017-10-01 23:56:30 +0200

    4

--- src/pojomarshallingxml/POJOMarshallingXML.java
+++ src/pojomarshallingxml/POJOMarshallingXML.java
@@ -22,17 +22,15 @@ public class POJOMarshallingXML {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Producto> pp = new ArrayList<>();
         Direccion direccion = new Direccion("Calle", "Luna", "13", "2ª", "b", "1", "2", "Jaén", 23002, "España", "Jaén");
         Cliente c1 = new Cliente("Angel", "Millán", "angelmillan@me.com", 606643217, "25987663D", 1, direccion);
         Producto p1 = new Producto(1, "Martillo Bellota", "Martillo de tapicero", 12.45);
         Producto p2 = new Producto(2,"Desatascador", "Destascador de chupón", 6.23);
         Producto p3 = new Producto(3,"Barrena 18", "Barrena de madera de 18 cm", 5.30);
-        pp.add(p1);
-        pp.add(p2);
-        pp.add(p3);
         Productos productos = new Productos();
-        productos.setProductos(pp);
+        productos.getProductos().add(p1);
+        productos.getProductos().add(p2);
+        productos.getProductos().add(p3);
         System.out.println(productos.toString());
 
         Factura factura = new Factura(c1, direccion, productos);
@@ -41,7 +39,7 @@ public class POJOMarshallingXML {
         
         
         try {
-            JAXBContext jbc = JAXBContext.newInstance(direccion1.getClass());
+            JAXBContext jbc = JAXBContext.newInstance(factura.getClass());
             Marshaller jbcm = jbc.createMarshaller();
             jbcm.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             jbcm.marshal(factura, new File("factura.xml"));
